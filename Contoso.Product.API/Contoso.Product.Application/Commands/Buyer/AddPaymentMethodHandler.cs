b6d27fcdb610597827a7f8675500aa8f10a7bef1
Calisto Caio <Caio.Calisto@pt.bosch.com>
2021-12-16 14:10:18 +0000

    ...

--- Contoso.Product.API/Contoso.Product.Application/Commands/Buyer/AddPaymentMethodHandler.cs
+++ Contoso.Product.API/Contoso.Product.Application/Commands/Buyer/AddPaymentMethodHandler.cs
@@ -1,14 +1,17 @@
-﻿using Contoso.Product.Infrastructure.Persistence;
+﻿using Contoso.Product.Infrastructure.Messaging;
+using Contoso.Product.Infrastructure.Persistence;
 
 namespace Contoso.Product.Application.Commands.Buyer
 {
     public class AddPaymentMethodHandler : IAddPaymentMethodHandler
     {
         private readonly IBuyerRepository buyerRepository;
+        private readonly IMessageBroker messageBroker;
 
-        public AddPaymentMethodHandler(IBuyerRepository buyerRepository)
+        public AddPaymentMethodHandler(IBuyerRepository buyerRepository, IMessageBroker messageBroker)
         {
             this.buyerRepository = buyerRepository;
+            this.messageBroker = messageBroker;
         }
 
         public Task AddPaymentMethod(AddPaymentMethod addPaymentMethod)
