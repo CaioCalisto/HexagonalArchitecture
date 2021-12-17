15c96e64f601da250633e9188111184759264417
Calisto Caio <Caio.Calisto@pt.bosch.com>
2021-12-17 15:09:07 +0000

    Integration Events

---
+++ Contoso.Product/Contoso.Product.Application/IntegrationEvents/BuyerCardExpired.cs
@@ -0,0 +1,9 @@
+﻿namespace Contoso.Product.Application.IntegrationEvents
+{
+    public class BuyerCardExpired
+    {
+        public Guid BuyerId { get; set; }
+
+        public string CardNumber { get; set; }
+    }
+}
